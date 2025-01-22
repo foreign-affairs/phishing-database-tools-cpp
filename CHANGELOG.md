cbb9638801d7465a43282245d1f8ab8a50d7821c
spirillen <44526987+spirillen@users.noreply.github.com>
2025-01-22 00:01:57 +0000

    Release 0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## Release 0.1.2
+- No comments, minor update
+
 ## Release 0.1.1
 - No comments, minor update
 
--- version.h
+++ version.h
@@ -5,6 +5,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "0.1.1"
+#define VERSION "0.1.2"
 
 #endif //VERSION_H
